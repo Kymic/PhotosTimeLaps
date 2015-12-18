9fb5003df7e1885071e324a91028bbd8af5d559c
Kymic <durey.vincent@gmail.com>
2015-12-18 19:45:27 +0100

    init

---
+++ PhotosTimeLaps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhotosTimeLaps.MvcApplication" Language="C#" %>
---
+++ PhotosTimeLaps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotosTimeLaps.Startup))]
+namespace PhotosTimeLaps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
